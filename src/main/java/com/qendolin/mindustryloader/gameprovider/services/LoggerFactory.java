c90a7e5de9e46b6c03effb35b835285100c61418
Qendolin <32160662+Qendolin@users.noreply.github.com>
2022-03-10 01:22:32 +0100

    Improve logger format

--- src/main/java/com/qendolin/mindustryloader/gameprovider/services/LoggerFactory.java
+++ src/main/java/com/qendolin/mindustryloader/gameprovider/services/LoggerFactory.java
@@ -6,13 +6,14 @@ import java.util.logging.*;
 
 public class LoggerFactory {
     private static final Formatter DEFAULT_FORMATTER = new SimpleFormatter() {
-        private static final String format = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
+        private static final String format = "[%1$tT] [%2$-7s] (%3$s) %4$s%n";
 
         @Override
         public synchronized String format(LogRecord lr) {
             return String.format(format,
                     new Date(lr.getMillis()),
-                    lr.getLevel().getLocalizedName(),
+                    lr.getLevel().getName(),
+                    lr.getLoggerName(),
                     lr.getMessage()
             );
         }
